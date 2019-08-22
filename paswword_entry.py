879562156c60f0a21cf9dd24532c3b9d46b65318
John Nurthen <john.nurthen@my.jcu.edu.au>
2019-08-22 11:38:26 +1000

    Add password entry program

--- paswword_entry.py
+++ paswword_entry.py
@@ -1,1 +1,10 @@
 '''John Nurthen'''
+min_length = int(3)
+password = input('Please enter your Password: ')
+password_length = len(password)
+while password_length < min_length:
+    print('Password must be between 3 and 10 characters')
+    password = input('Please enter a valid Password: ')
+    password_length = len(password)
+
+print('*' * password_length)
